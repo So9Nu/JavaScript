d08040eefb91049351549f1c20eca671d6febe98
Sonu jaiswal <jsonu9982@gmail.com>
2025-08-30 17:29:56 +0545

    Day3

---
+++ Chapter2/Comparison_Operators.js
@@ -0,0 +1,53 @@
+// equal to == 
+
+// equal to & type ===
+
+// not equal to !=
+
+// not equal to & type !==
+
+
+// let a=3;
+
+// if(a%2==0){
+//     console.log("even");
+// } else{
+//     console.log("odd");
+// }
+
+
+let a=5;
+let b=3;
+
+// a==b;
+console.log(a==b);
+
+
+{
+    // not equal to !=
+    let a=5;
+let b=3;
+
+
+console.log(a!=b);
+}
+{
+    //  equal to & type ===
+    // strict version.
+    let a=5;
+let b=5;
+
+
+console.log(a===b);
+}
+
+{
+    //  equal to & type ===
+    // strict version.
+    let a=5;
+let b="3";
+
+
+console.log(a!==b);
+}
+
---
+++ Chapter2/Logical_Ops.js
@@ -0,0 +1,31 @@
+//  And && 
+//   or ||
+//  not  !
+{
+//  And Operators
+    let a =6;
+let b=5;
+
+let cond1=a>b;
+let cond2=a===4;
+console.log(cond1&&cond2);
+}
+
+{
+//  Or Operators
+    let a =6;
+let b=5;
+
+let cond1=a<b;
+let cond2=a===4;
+console.log(cond1||cond2);
+}
+
+{
+//  Not Operators
+    let a =6;
+let b=5;
+
+
+console.log(!a<b);
+}
